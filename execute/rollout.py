1f220fe29c508957c441e5cbee26406565e96c4e
JackChan <jackchan9345@gmail.com>
2024-02-06 09:56:48 +0800

    Update rollout.py

--- execute/rollout.py
+++ execute/rollout.py
@@ -10,7 +10,7 @@ import copy
 from dataset.generate_dataset import sample_batch_task_id_cec21
 
 # inference for rollout
-
+# todo: fix
 
 class Memory():
     def __init__(self) -> None:
@@ -30,7 +30,6 @@ class Memory():
         del self.expr[:]
 
 
-# 两个考量方式：1. 与train时逻辑相同，看模仿的学习效果 2. 连续，看fitness
 def rollout(opts,agent,epoch,tb_logger,tokenizer,testing=False):
     # switch model's state to eval
     agent.set_evaling()
@@ -51,7 +50,6 @@ def rollout(opts,agent,epoch,tb_logger,tokenizer,testing=False):
         # teacher_env
         if opts.teacher=='madde':
             teacher_env_list=[lambda e=None: MadDE(dim=opts.dim,problem=e,max_x=opts.max_x,min_x=opts.min_x,max_fes=opts.max_fes) for i in range(opts.batch_size)]
-            
         elif opts.teacher=='cmaes':
             teacher_env_list=[lambda e=None: sep_CMA_ES(dim=opts.dim,problem=e,max_x=opts.max_x,min_x=opts.min_x,max_fes=opts.max_fes,sigma=opts.cmaes_sigma) for i in range(opts.batch_size)]
         elif opts.teacher=='pso':
@@ -69,15 +67,14 @@ def rollout(opts,agent,epoch,tb_logger,tokenizer,testing=False):
         task=TaskForTrain(learning_env,teacher_env,random_env,opts.batch_size,opts)
         
         
-        # for bat_id,batch_pro in enumerate(test_set):
         for bat_id,id in enumerate(test_id):
             # generate batch instances for testing
             instances,p_name=sample_batch_task_id_cec21(dim=opts.dim,batch_size=opts.batch_size,problem_id=id,seed=999)
 
                 
-            # only need one way to imitate
+            
             memory_imitate,bat_outperform_time,cost_stu,cost_base,cost_tea=rollout_imitate(opts,task,agent,tokenizer,instances,testing)
-            # memory_no_teacher=rollout_no_teacher(opts,task,model,tokenizer,bat_id)
+            
             test_outperform_time+=bat_outperform_time
 
             # for recording table
@@ -96,8 +93,6 @@ def rollout(opts,agent,epoch,tb_logger,tokenizer,testing=False):
             if not opts.no_tb:
                 tb_logger.add_scalars(f'performance/cost/{p_name}',{'student':cost_stu,'baseline':cost_base,'teacher':cost_tea},epoch)
 
-            
-
             # save data
             path=os.path.join(opts.data_saving_dir,f'epoch_{epoch}','test')
             if not os.path.exists(path):
@@ -107,7 +102,7 @@ def rollout(opts,agent,epoch,tb_logger,tokenizer,testing=False):
             np.save(os.path.join(path,f'batch_{bat_id}_{id}'),memory_imitate)
 
             
-            # log to tensorboard if needed，一个batch一张图，那就是八张图
+            # log opt figures
             if need_log:
                 log_to_test_with_teacher(memory_imitate.teacher_cost,memory_imitate.baseline_cost,memory_imitate.stu_cost,epoch,bat_id,id,tb_logger.file_writer.get_logdir(),logged=True)
                 # log_to_test_without_teacher(tb_logger,memory_imitate.teacher_cost,memory_no_teacher.stu_cost,epoch,bat_id,problem_id[bat_id])
@@ -137,8 +132,6 @@ def rollout(opts,agent,epoch,tb_logger,tokenizer,testing=False):
     
 
 # with teacher
-# 记录的cost，记录gap，记录outperform_ratio
-
 def rollout_imitate(opts,task,agent,tokenizer,instances,testing):
     # print(f'cur_problem:{bat_pro.__str__()}')
     
@@ -176,11 +169,8 @@ def rollout_imitate(opts,task,agent,tokenizer,instances,testing):
             seq,const_seq,log_prob=agent.actor(pop_feature)
         
         
-        # ! change: rollout的时候没必要记录reward，outperform ratio可以通过最后的优化结果计算
         target_pop,next_pop,baseline_pop,expr,is_done=task.step(stu_population,opts.skip_step,seq,const_seq,tokenizer,rand_seq,rand_c_seq,baseline_pop,testing=True)
         
-        
-
         memory.teacher_cost.append([p.gbest_cost for p in target_pop])
         memory.stu_cost.append([p.gbest_cost for p in next_pop])
         # memory.gap.append(gap)
@@ -189,7 +179,7 @@ def rollout_imitate(opts,task,agent,tokenizer,instances,testing):
 
         if is_done:
             break
-        # 定位到下一个种群
+        # next pop
         stu_population=next_pop
     outperform_time+=np.sum(memory.stu_cost[-1]<memory.baseline_cost[-1])
     # print(f'test outperform time:{outperform_time}')
